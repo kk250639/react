926b55aa9ab1430113da5ec02685aaaad420c887
kk250639 <chesyeo@gmail.com>
2025-07-04 16:28:57 +0900

    index : 하위 경로 없음

--- src/App52.jsx
+++ src/App52.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 
 function App52(props) {
   return (
@@ -34,6 +35,7 @@ function App52(props) {
                 </div>
               }
             />
+
             {/* /react/state */}
             <Route
               path="state"
@@ -51,3 +53,56 @@ function App52(props) {
 }
 
 export default App52;
+// import React from "react";
+//
+// function App52(props) {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route
+//             path="react"
+//             element={
+//               <div>
+//                 <div>NAVBAR</div>
+//                 <div>
+//                   <Outlet />
+//                 </div>
+//               </div>
+//             }
+//           >
+//             {/* index prop : 하위 경로 없음 */}
+//             <Route
+//               index
+//               element={
+//                 <div>
+//                   <h3>HOME</h3>
+//                 </div>
+//               }
+//             />
+//             {/* /react/learn */}
+//             <Route
+//               path="learn"
+//               element={
+//                 <div>
+//                   <h3>LEARN</h3>
+//                 </div>
+//               }
+//             />
+//             {/* /react/state */}
+//             <Route
+//               path="state"
+//               element={
+//                 <div>
+//                   <h3>STATE</h3>
+//                 </div>
+//               }
+//             />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+//
+// export default App52;

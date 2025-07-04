153df41730b494921c1898f98d44b58f049f9193
kk250639 <chesyeo@gmail.com>
2025-07-04 10:33:52 +0900

    b

--- src/App41.jsx
+++ src/App41.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function BoardList() {
   return (
     <div>
-      <h3>게시판 목록</h3>
+      <h3>게시물 목록</h3>
     </div>
   );
 }
@@ -27,15 +28,56 @@ function About() {
 function App41(props) {
   return (
     <div>
-      <BrowerRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="board/list" element={<BoardList />}></Route>
           <Route path="member/list" element={<MemberList />}></Route>
           <Route path="about" element={<About />}></Route>
         </Routes>
-      </BrowerRouter>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App41;
+// import React from "react";
+//
+// function BoardList() {
+//   return (
+//     <div>
+//       <h3>게시판 목록</h3>
+//     </div>
+//   );
+// }
+//
+// function MemberList() {
+//   return (
+//     <div>
+//       <h3>회원 목록</h3>
+//     </div>
+//   );
+// }
+//
+// function About() {
+//   return (
+//     <div>
+//       <h3>어바웃 정보</h3>
+//     </div>
+//   );
+// }
+//
+// function App41(props) {
+//   return (
+//     <div>
+//       <BrowerRouter>
+//         <Routes>
+//           <Route path="board/list" element={<BoardList />}></Route>
+//           <Route path="member/list" element={<MemberList />}></Route>
+//           <Route path="about" element={<About />}></Route>
+//         </Routes>
+//       </BrowerRouter>
+//     </div>
+//   );
+// }
+//
+// export default App41;

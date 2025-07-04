40145837eef57f6f7a56ebdfd664bbf839e4c427
kk250639 <chesyeo@gmail.com>
2025-07-04 12:48:39 +0900

    검색기능/ 페이지 이동 기능

--- src/App46.jsx
+++ src/App46.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { useSearchParams } from "react-router";
+import { BrowserRouter, Route, Routes, useSearchParams } from "react-router";
 
 // /board/detail?id=3 : <h3>3번 게시물</h3>
-// /board/detail?id=13 : <h3>13번 게시물</h3>
+// /board/detail?id=13 : <h3>3번 게시물</h3>
 // /board/detail?id=33 : <h3>33번 게시물</h3>
+
+// 연습 :
+// /member/detail?id=son : <h3>son 회원 정보 보기 </h3>
+// /member/detail?id=park : <h3>park 회원 정보 보기 </h3>
+// /member/detail?id=cha : <h3>cha 회원 정보 보기 </h3>
 
 function Child1() {
   // useSearchParams hook : query string(request param) 정보 얻기
@@ -20,16 +25,67 @@ function Child1() {
   );
 }
 
+function Child2() {
+  const [params] = useSearchParams();
+  console.log(params);
+  console.log("id", params.get("id"));
+  return (
+    <div>
+      <h3>{params.get("id")} 회원 정보 찾기</h3>
+    </div>
+  );
+}
+
 function App46(props) {
   return (
     <div>
-      <BrowerRoute>
+      <BrowserRouter>
         <Routes>
-          <Route path="board/detail" element={<Child1 />}></Route>
+          <Route path="board">
+            <Route path="detail" element={<Child1 />} />
+          </Route>
+          <Route path="member">
+            <Route path="detail" element={<Child2 />} />
+          </Route>
         </Routes>
-      </BrowerRoute>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App46;
+// import React from "react";
+// import { useSearchParams } from "react-router";
+//
+// // /board/detail?id=3 : <h3>3번 게시물</h3>
+// // /board/detail?id=13 : <h3>13번 게시물</h3>
+// // /board/detail?id=33 : <h3>33번 게시물</h3>
+//
+// function Child1() {
+//   // useSearchParams hook : query string(request param) 정보 얻기
+//   // URLSearchParams와 set 메소드를 배열로 리턴
+//   const [params] = useSearchParams();
+//
+//   console.log(params);
+//   console.log("id", params.get("id"));
+//
+//   return (
+//     <div>
+//       <h3>{params.get("id")} 번 게시물 보기</h3>
+//     </div>
+//   );
+// }
+//
+// function App46(props) {
+//   return (
+//     <div>
+//       <BrowerRoute>
+//         <Routes>
+//           <Route path="board/detail" element={<Child1 />}></Route>
+//         </Routes>
+//       </BrowerRoute>
+//     </div>
+//   );
+// }
+//
+// export default App46;

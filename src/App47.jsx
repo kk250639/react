0d810120945a2c3f242d3c9124fe98d7cd04b5b1
kk250639 <chesyeo@gmail.com>
2025-07-04 14:21:06 +0900

    냅바

--- src/App47.jsx
+++ src/App47.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes, useSearchParams } from "react-router";
+import { Button } from "react-bootstrap";
 
 function Child1() {
   const [keyword, setKeyword] = useState("");
   const [params, setSearchParams] = useSearchParams();
-
   function handleSearchClick() {
     setSearchParams({ keyword: keyword });
   }
@@ -42,21 +42,20 @@ function Child1() {
     </div>
   );
 }
+// 연습 :  검색기능, 페이지 이동 기능이 있는 Child2 만들기
 
-// 연습 : 검색기능, 페이지 이동 기능이 있는 Child2 만들기
 function Child2() {
   const [keyword, setKeyword] = useState("");
-  const [params, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pages = [1, 2, 3, 4, 5];
 
   function handleSearchClick() {
     setSearchParams({ keyword: keyword });
   }
 
-  const pageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  function handlePageButtonClick(num) {
-    console.log(num + "로 이동");
-    const nextSearchParams = new URLSearchParams(params);
+  function handlePageClick(num) {
+    const nextSearchParams = new URLSearchParams(searchParams);
     nextSearchParams.set("page", num);
     setSearchParams(nextSearchParams);
   }
@@ -65,35 +64,31 @@ function Child2() {
     <div>
       <input
         type="text"
-        placeholder="검색어"
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
       />
       <button onClick={handleSearchClick}>검색</button>
       <hr />
-      <div className="d-flex justify-content-between">
-        {pageNumbers.map((num) => (
-          <div
-            style={{ cursor: "pointer" }}
-            onClick={() => handlePageButtonClick(num)}
-            key={num}
-          >
-            {num}
-          </div>
-        ))}
-      </div>
+      {pages.map((num) => (
+        <Button
+          onClick={() => handlePageClick(num)}
+          variant="outline-primary"
+          key={num}
+        >
+          {num}
+        </Button>
+      ))}
     </div>
   );
 }
 
-//
 function App47(props) {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/board/list" element={<Child1 />}></Route>
-          <Route path="/member/list" element={<Child2 />}></Route>
+          <Route path="/board/list" element={<Child1 />} />
+          <Route path="/member/list" element={<Child2 />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -101,3 +96,106 @@ function App47(props) {
 }
 
 export default App47;
+// import React, { useState } from "react";
+// import { BrowserRouter, Route, Routes, useSearchParams } from "react-router";
+//
+// function Child1() {
+//   const [keyword, setKeyword] = useState("");
+//   const [params, setSearchParams] = useSearchParams();
+//
+//   function handleSearchClick() {
+//     setSearchParams({ keyword: keyword });
+//   }
+//
+//   const pageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+//   function handlePageButtonClick(num) {
+//     console.log(num + "로 이동");
+//     const nextSearchParams = new URLSearchParams(params);
+//     nextSearchParams.set("page", num);
+//     setSearchParams(nextSearchParams);
+//   }
+//
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="검색어"
+//         value={keyword}
+//         onChange={(e) => setKeyword(e.target.value)}
+//       />
+//       <button onClick={handleSearchClick}>검색</button>
+//       <hr />
+//       <div className="d-flex justify-content-between">
+//         {pageNumbers.map((num) => (
+//           <div
+//             style={{ cursor: "pointer" }}
+//             onClick={() => handlePageButtonClick(num)}
+//             key={num}
+//           >
+//             {num}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+//
+// // 연습 : 검색기능, 페이지 이동 기능이 있는 Child2 만들기
+// function Child2() {
+//   const [keyword, setKeyword] = useState("");
+//   const [params, setSearchParams] = useSearchParams();
+//
+//   function handleSearchClick() {
+//     setSearchParams({ keyword: keyword });
+//   }
+//
+//   const pageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+//   function handlePageButtonClick(num) {
+//     console.log(num + "로 이동");
+//     const nextSearchParams = new URLSearchParams(params);
+//     nextSearchParams.set("page", num);
+//     setSearchParams(nextSearchParams);
+//   }
+//
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="검색어"
+//         value={keyword}
+//         onChange={(e) => setKeyword(e.target.value)}
+//       />
+//       <button onClick={handleSearchClick}>검색</button>
+//       <hr />
+//       <div className="d-flex justify-content-between">
+//         {pageNumbers.map((num) => (
+//           <div
+//             style={{ cursor: "pointer" }}
+//             onClick={() => handlePageButtonClick(num)}
+//             key={num}
+//           >
+//             {num}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+//
+// //
+// function App47(props) {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/board/list" element={<Child1 />}></Route>
+//           <Route path="/member/list" element={<Child2 />}></Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+//
+// export default App47;

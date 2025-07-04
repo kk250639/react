9da39c6e0839bea83985c03a09f797c72d63c64e
kk250639 <chesyeo@gmail.com>
2025-07-04 15:47:51 +0900

    컴포넌트 없이 경로작성

--- src/App50.jsx
+++ src/App50.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+function MainLayout() {
+  return (
+    <div>
+      <div>
+        <div>NAVBAR</div>
+        <div>
+          <Link to="/abc">abc</Link>
+        </div>
+        <div>
+          <Link to="/xyz">xyz</Link>
+        </div>
+        <div>
+          <Link to="/qwe">qwe</Link>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
 
 function App50(props) {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route
+              path="abc"
+              element={
+                <div>
+                  <h3></h3>
+                </div>
+              }
+            />
+            <Route
+              path="xyz"
+              element={
+                <div>
+                  <h3></h3>
+                </div>
+              }
+            />
+            <Route
+              path="qwe"
+              element={
+                <div>
+                  <h3></h3>
+                </div>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App50;

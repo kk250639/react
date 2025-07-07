7c4bd3021601c1404a28d2d68b2941c730c91adf
kk250639 <chesyeo@gmail.com>
2025-07-07 09:23:28 +0900

    axios 설정 /api로 시작하는 경로는 "http://localhost:8080"를 붙이기

--- src/App53.jsx
+++ src/App53.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import axios from "axios";
 
 function App53(props) {
-  return <div></div>;
+  function handleButton1Click() {
+    axios.request(config:{
+      method: "get",
+        url: "http://localhost:8080/main1/sub1"});
+  }
+
+  return (
+    <div>
+      <button onClick={handleButton1Click}>요청 보내기 1</button>
+    </div>
+  );
 }
 
 export default App53;
